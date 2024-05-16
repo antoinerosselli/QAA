a702fb1b33165e58d682e43030630efdcb5413ca
antowaan <76961964+antoinerosselli@users.noreply.github.com>
2024-05-16 11:50:18 +0200

    Quick Fix
    change welcome message

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="home-container">
-      <h1>Welcome to the Open Source Project</h1>
+      <h1>Welcome to QAA (feur)</h1>
       <p>This project aims to create a platform to facilitate work in the video game industry.</p>
       <p>It is completely open source, and we welcome contributions from developers around the world.</p>
       <a href="https://github.com/antoinerosselli/QAA" target="_blank" rel="noopener noreferrer" className="github-button">
